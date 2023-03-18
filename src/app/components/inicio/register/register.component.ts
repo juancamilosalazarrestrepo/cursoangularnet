7eda6d8bc6bf74a310d108d38700db0d92150e8e
juan salazar <juancamilosalazarrestrepo@gmail.com>
2023-03-18 10:01:45 -0500

    Componente register , creado

--- src/app/components/inicio/register/register.component.ts
+++ src/app/components/inicio/register/register.component.ts
@@ -1,10 +1,23 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-register',
   templateUrl: './register.component.html',
-  styleUrls: ['./register.component.css']
+  styleUrls: ['./register.component.css'],
 })
 export class RegisterComponent {
+  register: FormGroup;
 
+  constructor(private fb: FormBuilder) {
+    this.register = this.fb.group({
+      usuario: ['', Validators.required],
+      password: ['', Validators.required],
+    });
+  }
+
+  ngOnInit(): void {
+    //Called after the constructor, initializing input properties, and the first call to ngOnChanges.
+    //Add 'implements OnInit' to the class.
+  }
 }
