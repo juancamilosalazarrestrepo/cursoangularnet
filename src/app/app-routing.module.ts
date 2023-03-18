7d867cb13b6dd32b34394f628235a00e0b34ca27
juan salazar <juancamilosalazarrestrepo@gmail.com>
2023-03-18 13:48:42 -0500

    reactorizacion routes module

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,20 +1,38 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { CambiarPasswordComponent } from './components/dashboard/cambiar-password/cambiar-password.component';
+import { CuestionariosComponent } from './components/dashboard/cuestionarios/cuestionarios.component';
+import { DashboardComponent } from './components/dashboard/dashboard.component';
 import { BienvenidaComponent } from './components/inicio/bienvenida/bienvenida.component';
+import { InicioComponent } from './components/inicio/inicio.component';
 import { LoginComponent } from './components/inicio/login/login.component';
 import { RegisterComponent } from './components/inicio/register/register.component';
 
 const routes: Routes = [
-  {path:"", redirectTo:"/bienvenidos" , pathMatch: "full"},
-  {path:"bienvenidos", component:BienvenidaComponent},
-  {path:"register", component:RegisterComponent},
-  {path:"login", component:LoginComponent},
-  {path:"**", redirectTo:'/bienvenidos', pathMatch:"full"},
+  { path: '', redirectTo: '/inicio', pathMatch: 'full' },
+  {
+    path: 'inicio',
+    component: InicioComponent,
+    children: [
+      { path: '', component: BienvenidaComponent },
+      { path: 'register', component: RegisterComponent },
+      { path: 'login', component: LoginComponent },
+    ],
+  },
+  {
+    path: 'dashboard',
+    component: DashboardComponent,
+    children: [
+      { path: '', component: CuestionariosComponent },
+      { path: 'cambiarPassword', component: CambiarPasswordComponent },
+    ],
+  },
 
+  { path: '**', redirectTo: '/bienvenidos', pathMatch: 'full' },
 ];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
--- src/app/components/inicio/register/register.component.ts
+++ src/app/components/inicio/register/register.component.ts
@@ -10,14 +10,29 @@ export class RegisterComponent {
   register: FormGroup;
 
   constructor(private fb: FormBuilder) {
-    this.register = this.fb.group({
-      usuario: ['', Validators.required],
-      password: ['', Validators.required],
-    });
+    this.register = this.fb.group(
+      {
+        usuario: ['', Validators.required],
+        password: ['', [Validators.required, Validators.minLength(4)]],
+        confirmPassword: [''],
+      },
+      { validator: this.checkPassword }
+    );
   }
 
   ngOnInit(): void {
     //Called after the constructor, initializing input properties, and the first call to ngOnChanges.
     //Add 'implements OnInit' to the class.
+  }
+
+  registrarUsuario(): void {
+    console.log(this.register);
+  }
+
+  checkPassword(group: FormGroup): any {
+    const pass = group.controls['password'].value;
+    const confirmPassword = group.controls['confirmPassword'].value;
+
+    return pass === confirmPassword ? null : { notSame: true };
   }
 }
