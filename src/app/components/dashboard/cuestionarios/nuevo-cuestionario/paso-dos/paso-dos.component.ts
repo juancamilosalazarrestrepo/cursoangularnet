13c33a5650cf580b241b5e5d25207c7bb8d1e365
juan salazar <juancamilosalazarrestrepo@gmail.com>
2023-08-23 00:40:49 -0500

    creacion del formulario de nueva pregunta

--- src/app/components/dashboard/cuestionarios/nuevo-cuestionario/paso-dos/paso-dos.component.ts
+++ src/app/components/dashboard/cuestionarios/nuevo-cuestionario/paso-dos/paso-dos.component.ts
@@ -1,21 +1,29 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+import { ToastrService } from 'ngx-toastr';
+import { Pregunta } from 'src/app/models/pregunta';
 import { CuestionarioService } from 'src/app/services/cuestionario.service';
 
 @Component({
   selector: 'app-paso-dos',
   templateUrl: './paso-dos.component.html',
-  styleUrls: ['./paso-dos.component.css']
+  styleUrls: ['./paso-dos.component.css'],
 })
 export class PasoDosComponent {
   tituloCuestionario: string;
   descripcionCuestionario: string;
-constructor(private cuestionarioService: CuestionarioService) { 
-  this.tituloCuestionario = ""
-  this.descripcionCuestionario = ""
-
-}
-ngOnInit(): void {
-this.tituloCuestionario = this.cuestionarioService.tituloCuestionario;
-this.descripcionCuestionario = this.cuestionarioService.descripcionCuestionario;
-}
+  listPreguntas: Pregunta[] = [];
+  constructor(
+    private cuestionarioService: CuestionarioService,
+    private toastr: ToastrService,
+    private router: Router
+  ) {
+    this.tituloCuestionario = '';
+    this.descripcionCuestionario = '';
+  }
+  ngOnInit(): void {
+    this.tituloCuestionario = this.cuestionarioService.tituloCuestionario;
+    this.descripcionCuestionario =
+      this.cuestionarioService.descripcionCuestionario;
+  }
 }
