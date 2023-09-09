9b8e96de25fcaf1a1794652a7fced8a6a11fb90c
juan salazar <juancamilosalazarrestrepo@gmail.com>
2023-09-09 14:44:44 -0500

    traer datos del cuestionario

--- src/app/components/inicio/list-cuestionarios/pregunta/pregunta.component.ts
+++ src/app/components/inicio/list-cuestionarios/pregunta/pregunta.component.ts
@@ -1,10 +1,31 @@
 import { Component } from '@angular/core';
+import { CuestionarioService } from 'src/app/services/cuestionario.service';
+import { RespuestaCuestionarioService } from 'src/app/services/respuesta-cuestionario.service';
 
 @Component({
   selector: 'app-pregunta',
   templateUrl: './pregunta.component.html',
-  styleUrls: ['./pregunta.component.css']
+  styleUrls: ['./pregunta.component.css'],
 })
 export class PreguntaComponent {
+  idCuestionario: number;
+  constructor(
+    private repsuestaCuestionario: RespuestaCuestionarioService,
+    private cuestionarioService: CuestionarioService
+  ) {
+    this.idCuestionario = 0;
+  }
+  ngOnInit(): void {
+    this.idCuestionario = this.repsuestaCuestionario.idcuestionario;
+    //Called after the constructor, initializing input properties, and the first call to ngOnChanges.
+    //Add 'implements OnInit' to the class.
+    console.log(this.repsuestaCuestionario.idcuestionario);
+    this.getCuestionario();
+  }
 
+  getCuestionario():void{
+    this.cuestionarioService.getCuestionario(this.idCuestionario).subscribe(data=>{
+      console.log(data)
+    })
+  }
 }
